f5a4811c88e27001f0da64d44afd9d1dd1bd1b53
Vale-2 <valerijbalakaev3@gmail.com>
2023-06-17 16:23:59 +0300

    main

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 
-
-def str_counter(s):
-
-    for i in s:
-        counter = 0
-        for io in s:
-            if i == io:
-                counter +=1
-        print(i, counter)
-
-str_counter('asddssddf')
+def pal(str1):
+    if str1 == str1[::-1]:
+        print(True)
+    else:
+        print(False)
