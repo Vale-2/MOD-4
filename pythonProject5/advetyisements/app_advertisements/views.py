2aa07d0a90688fe658cebc0ae325bc674fd06a43
Vale-2 <valerijbalakaev3@gmail.com>
2023-07-20 21:14:13 +0300

    Update views.py

--- pythonProject5/advetyisements/app_advertisements/views.py
+++ pythonProject5/advetyisements/app_advertisements/views.py
@@ -2,5 +2,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(response):
-    return HttpResponse('Success!!')
+def index(request):
+    return render(request,'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
