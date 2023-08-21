1df53cedaec6d27a5f4a8ef9a2b76401efc180a3
Vale-2 <valerijbalakaev3@gmail.com>
2023-08-21 20:24:54 +0300

    Update views.py

--- pythonProject5/advetyisements/app_advertisements/views.py
+++ pythonProject5/advetyisements/app_advertisements/views.py
@@ -1,20 +1,29 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
+from .models import Advertisement
+from .forms import AdvertisementForm
 
 def index(request):
-    return render(request,'index.html')
+    advertisements = Advertisement.objects.all()
+    contex = {'advertisements':advertisements}
+    return render(request, 'index.html', contex)
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
 
-def profile(request):
-    return render(request, 'profile.html')
-def advertisement_post(request):
-    return render(request, 'advertisement-post.html')
-
 def register(request):
     return render(request, 'register.html')
 
-def login_post(request):
-    return render(request, 'login.html')
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement=Advertisement(**form.cleaned_data)
+            advertisement.user=request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form':form}
+    return render(request, 'advertisement-post.html', context)
