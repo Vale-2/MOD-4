31b4e1c12dd8b0223877204987129cad248458da
Vale-2 <valerijbalakaev3@gmail.com>
2023-08-26 18:48:38 +0300

    Update urls.py

--- pythonProject5/advetyisements/app_advertisements/urls.py
+++ pythonProject5/advetyisements/app_advertisements/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import index, top_sellers, register, advertisement_post
+from .views import index, top_sellers, advertisement_post
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('register/', register, name = 'register'),
+    path('', index, name='main-page'),
     path('top-sellers/', top_sellers, name = 'top-sellers'),
-    path('advertisement_post/', advertisement_post, name = 'advertisement_post'),
-#    path('login_post', login_post, name = 'login_post'),
-#    path('profile/', profile, name = 'profile'),
+
+    path('advertisement-post/', advertisement_post, name = 'advertisement-post'),
 ]
+
